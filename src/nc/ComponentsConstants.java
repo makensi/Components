ae7aac703cdb33c8ba498cc5d58c0c1c0abfc3af
makensi <makensi@gmail.com>
2012-01-26 23:02:27 +0100

    Remove name accent

--- src/nc/ComponentsConstants.java
+++ src/nc/ComponentsConstants.java
@@ -1,5 +1,5 @@
 /*
-	Copyright 2012 Raœl de la Hoz Garrido
+	Copyright 2012 Raul de la Hoz Garrido
 
 	Licensed under the Apache License, Version 2.0 (the "License");
 	you may not use this file except in compliance with the License.
