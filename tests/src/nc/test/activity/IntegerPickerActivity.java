b64e27d2815740025b3cdec58af7ef895c89a1a5
makensi <makensi@gmail.com>
2012-01-27 07:37:30 +0100

    test package refactor

--- tests/src/nc/test/activity/IntegerPickerActivity.java
+++ tests/src/nc/test/activity/IntegerPickerActivity.java
@@ -13,7 +13,7 @@
 	See the License for the specific language governing permissions and
 	limitations under the License.
 */
-package nc.activity;
+package nc.test.activity;
 
 import nc.test.R;
 import android.app.Activity;
